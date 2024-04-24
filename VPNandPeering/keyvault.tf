7742c3e86b37e481ed13fee22373e1bc593d059b
Tino <119343176+tinobks@users.noreply.github.com>
2024-04-24 16:29:05 +0200

    Update keyvault.tf

--- VPNandPeering/keyvault.tf
+++ VPNandPeering/keyvault.tf
@@ -5,7 +5,7 @@ data "azurerm_key_vault_secret" "examplesecret" {
 
     #modificare mettendo la propria sub id, nome del rg dove risiede la keyvault e nome della keyvault
     #key_vault_id = "/subscriptions/<subscription_id>/resourceGroups/<nome_rg>/providers/Microsoft.KeyVault/vaults/<nome_keyvault>"
-    key_vault_id = "/subscriptions/b5f44762-6dd4-4029-ae3e-e40e216478b7/resourceGroups/tb-rg/providers/Microsoft.KeyVault/vaults/tbkeyvault24042024"
+    key_vault_id = "/subscriptions/xxxxxxxxx-xxxxx-xxxx-xxxx-xxxxxxxxxxxx/resourceGroups/tb-rg/providers/Microsoft.KeyVault/vaults/tbkeyvault24042024"
 }
 
 output "secretvalue" {
